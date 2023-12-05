c126fad99d7cd2568a457c4d6de6db011ba1cfd7
zsl99a <zsl99a@live.com>
2023-12-05 17:34:09 +0800

    处理MtlsProvider没有实现Send报错的问题

--- nitrogen-quic/src/quic.rs
+++ nitrogen-quic/src/quic.rs
@@ -9,21 +9,13 @@ pub static MY_CERT_PEM: &str = concat!(env!("CARGO_MANIFEST_DIR"), "/certs/serve
 pub static MY_KEY_PEM: &str = concat!(env!("CARGO_MANIFEST_DIR"), "/certs/server.key");
 
 pub async fn create_client(addr: SocketAddr) -> anyhow::Result<Client> {
-    let client = Client::builder()
-        .with_event(Subscriber::default())?
-        .with_tls(MtlsProvider::new(CA_CERT_PEM, MY_CERT_PEM, MY_KEY_PEM).await?)?
-        .with_io(addr)?
-        .start()?;
-
+    let mtls = MtlsProvider::new(CA_CERT_PEM, MY_CERT_PEM, MY_KEY_PEM).await?;
+    let client = Client::builder().with_event(Subscriber::default())?.with_tls(mtls)?.with_io(addr)?.start()?;
     Ok(client)
 }
 
 pub async fn create_server(addr: SocketAddr) -> anyhow::Result<Server> {
-    let server = Server::builder()
-        .with_event(Subscriber::default())?
-        .with_tls(MtlsProvider::new(CA_CERT_PEM, MY_CERT_PEM, MY_KEY_PEM).await?)?
-        .with_io(addr)?
-        .start()?;
-
+    let mtls = MtlsProvider::new(CA_CERT_PEM, MY_CERT_PEM, MY_KEY_PEM).await?;
+    let server = Server::builder().with_event(Subscriber::default())?.with_tls(mtls)?.with_io(addr)?.start()?;
     Ok(server)
 }
